0211bf099b9f6a1c66f683a8ed4f98a3b9e14926
Sun <astrolithium@gmail.com>
2024-02-08 02:59:13 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './Components/Nav';
+import HomeCaro from './Components/HomeCaro';
+import '@mantine/core/styles.css';
+import HomePage from './HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>
 );
 
